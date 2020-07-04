cc21ce99b98f672287633d93988a0fbcf07a0e8b
Tiunchik <senebh@gmail.com>
2020-07-04 20:33:50 +0300

    add additional annotations to configuration file

--- src/main/java/org/chess/library/monitoring/Config.java
+++ src/main/java/org/chess/library/monitoring/Config.java
@@ -2,8 +2,10 @@ package org.chess.library.monitoring;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan
+@EnableJpaRepositories
 public class Config {
 }

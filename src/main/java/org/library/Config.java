3437e5581245631783e737cc1f387402db624f90
Tiunchik <senebh@gmail.com>
2020-07-06 21:47:35 +0300

    change Config configuration method from ComponentScan to Beans and EnableJpaRep

--- src/main/java/org/library/Config.java
+++ src/main/java/org/library/Config.java
@@ -6,18 +6,15 @@ import org.library.monitoring.MonitoringEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories(basePackageClasses = org.library.monitoring.MonitorEventRepository.class)
 public class Config {
 
     @Bean
     public MonitoringEvent monitoringEvent() {
         return new MonitoringEvent();
-    }
-
-    @Bean(name = {"eventRepository", "myRepo"})
-    public MonitorEventRepository monitorEventRepository(MonitorEventRepository eventRepository) {
-        return eventRepository;
     }
 
     @Bean

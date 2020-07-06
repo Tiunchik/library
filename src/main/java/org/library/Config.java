25227512077987b1e13cc1b5db5fdc843830bac5
Tiunchik <senebh@gmail.com>
2020-07-06 21:01:05 +0300

    change Config.class ComponentScan settings

--- src/main/java/org/library/Config.java
+++ src/main/java/org/library/Config.java
@@ -1,9 +1,17 @@
 package org.library;
 
+import org.library.monitoring.MonitorEventRepository;
+import org.library.monitoring.Monitoring;
+import org.library.monitoring.MonitoringAspect;
+import org.library.monitoring.MonitoringEvent;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"org.library.*"})
+@ComponentScan(basePackageClasses = {MonitoringEvent.class,
+        MonitorEventRepository.class,
+        MonitoringAspect.class,
+        Monitoring.class})
 public class Config {
+
 }

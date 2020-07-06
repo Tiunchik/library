dffc87c49f7299266f9b8163e486c38464997748
Tiunchik <senebh@gmail.com>
2020-07-06 21:59:13 +0300

    change Config configuration

--- src/main/java/org/library/Config.java
+++ src/main/java/org/library/Config.java
@@ -1,15 +1,14 @@
 package org.library;
 
-import org.library.monitoring.MonitorEventRepository;
 import org.library.monitoring.MonitoringAspect;
 import org.library.monitoring.MonitoringEvent;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@ComponentScan(basePackageClasses = {MonitoringEvent.class})
+@EntityScan(basePackageClasses = {MonitoringEvent.class})
 @EnableJpaRepositories(basePackageClasses = org.library.monitoring.MonitorEventRepository.class)
 @ComponentScan(basePackageClasses = {MonitoringAspect.class})
 public class Config {

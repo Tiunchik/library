cf7771974f12dd5aa42aae7a1fc9370e4f8d2cb7
Tiunchik <senebh@gmail.com>
2020-07-06 21:32:42 +0300

    change Config configuration method from ComponentScan to Beans

--- src/main/java/org/library/Config.java
+++ src/main/java/org/library/Config.java
@@ -1,5 +1,9 @@
 package org.library;
 
+import org.library.monitoring.MonitorEventRepository;
+import org.library.monitoring.MonitoringAspect;
+import org.library.monitoring.MonitoringEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,4 +11,18 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class Config {
 
+    @Bean
+    public MonitoringEvent monitoringEvent() {
+        return new MonitoringEvent();
+    }
+
+    @Bean(name = {"eventRepository", "myRepo"})
+    public MonitorEventRepository monitorEventRepository(MonitorEventRepository eventRepository) {
+        return eventRepository;
+    }
+
+    @Bean
+    public MonitoringAspect monitoringAspect(MonitorEventRepository eventRepository) {
+        return new MonitoringAspect(eventRepository);
+    }
 }
